cba784b0b55086c0adb19b501967b2d1c23117fb
ghfjfjdck <157564400+ghfjfjdck@users.noreply.github.com>
2024-12-02 15:44:40 +0500

    Add files via upload

---
+++ zd6_1_BelozerovKlim/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Xamarin.Forms;
+
+namespace zd6_1_BelozerovKlim
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnNavigateButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
+    
+
